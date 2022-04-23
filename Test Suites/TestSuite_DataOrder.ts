d295eb0798c9105a9a8fc3c6ffdb866581c0b147
ratnaasr <99734231+ratnaasr@users.noreply.github.com>
2022-04-23 14:52:34 +0800

    Add Data Order

---
+++ Test Suites/TestSuite_DataOrder.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite_DataOrder</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>93366e47-ae99-46b5-a8e1-b2a740a0699c</testSuiteGuid>
+   <testCaseLink>
+      <guid>5ded9dc2-d189-4f58-ab94-015cefdbb2fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TS_Data_Orderan/TC01_Antrian</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7a02f265-44c3-4d82-ae8d-d281ebb0bcaf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TS_Data_Orderan/TC02_Diproses</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9fce830a-19de-4654-8ac9-c2d69af2453a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TS_Data_Orderan/TC03_Selesai</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>59c1f10d-4184-4a8e-b636-6e13fce388d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TS_Data_Orderan/TC04_Delete Orderan</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
