14d67e913d60a94cd9e536ef7e12e6b6e500613d
ASUS <ASUS@DESKTOP-OAF2AN5>
2022-04-23 12:04:25 +0700

    Add Produk

---
+++ Test Suites/TestSuite_Produk.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite_Produk</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e4bb5ec7-85ba-4a57-8cad-c66a206a674c</testSuiteGuid>
+   <testCaseLink>
+      <guid>3d146e0c-6955-44c0-9cc2-18f7c545ee27</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01_Menambah_edit_hapus_produk/TC01_Menambah data per Kg</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4066c199-00bc-48e6-a95f-77f7c9ef8721</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01_Menambah_edit_hapus_produk/TC02_Menambah data Jumlah</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8869ca6-4cd1-4ffb-949b-d03508a77cfa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01_Menambah_edit_hapus_produk/TC03_Edit Produk</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>77883ca9-b592-4027-8b4b-170be32f264f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01_Menambah_edit_hapus_produk/TC04_Hapus Produk</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
