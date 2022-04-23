91b9443f56d955a2da044eb84ab09c9e1b9f654c
izzazainf <izzazain25@gmail.com>
2022-04-23 14:20:46 +0700

    Dummy data for order

--- Test Suites/Dump_ForRunOrder.ts
+++ Test Suites/Dump_ForRunOrder.ts
@@ -5,10 +5,60 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>7dc807d0-d037-4b38-82bb-b8ff84a39724</testSuiteGuid>
+   <testCaseLink>
+      <guid>9ff99833-d7b2-4524-bcab-337c9552cd42</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MR-Order/AddListPelanggan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ec6eaa11-f85d-43df-a6de-bbdf0218e95d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ad569108-c48e-4059-a843-cbbe1058277f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>af95821d-2b9e-468c-8220-8a5bc7dc7c25</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a3f0d061-d8f1-407d-afda-2cf8a4a2848e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MR-Order/AddListProduk</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2b0793c8-8bfd-40a3-809e-62ea608f7a52</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b267c48f-afd8-46e4-aa37-1ea561748255</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ea9afc1c-5b00-402e-a961-c4d57ccf6c03</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
